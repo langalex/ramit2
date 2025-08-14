7ec4a646c77eae054c468f5c0aac6154a07cafc4
Alexander Lang <alex@cobot.me>
2025-08-14 21:49:07 +0200

    add night mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import SiteHeader from '$lib/components/ui/site-header.svelte';
 	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
@@ -11,6 +12,7 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<ModeWatcher />
 <SiteHeader />
 <ScrollArea class="h-full	p-4">
 	{@render children?.()}
