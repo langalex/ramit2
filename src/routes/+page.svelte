7d394fc5e5290b9331dc5ba630902082528aac9e
Alexander Lang <alex@cobot.me>
2025-08-14 21:40:31 +0200

    focus field when adding account

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,19 @@
 <script lang="ts">
 	import * as Table from '$lib/components/ui/table/index.js';
-	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
-	import * as Drawer from '$lib/components/ui/drawer/index.js';
-	import { Input } from '$lib/components/ui/input/index.js';
-	import { Label } from '$lib/components/ui/label/index.js';
-	import { create as createAccount } from '$lib/models/account.svelte';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+
 	import { resolve } from '$app/paths';
+	import AddAccountDrawer from './AddAccountDrawer.svelte';
 
 	const { data } = $props();
 	const { balancesByAccount } = data;
 	const accounts = $derived([...data.accounts].sort((a, b) => a.name.localeCompare(b.name)));
 
-	const id = $props.id();
 	let name = $state('');
 	let showAddAccountDrawer = $state(false);
 	const total = $derived(
 		Object.values(balancesByAccount).reduce((acc, balance) => acc + balance, 0)
 	);
-
-	async function addAccount(event: Event) {
-		event.preventDefault();
-		await createAccount(name);
-		showAddAccountDrawer = false;
-		name = '';
-	}
 </script>
 
 <Table.Root>
@@ -48,31 +38,11 @@
 </Table.Root>
 
 <div class="m-auto w-full p-4">
-	<Drawer.Root bind:open={showAddAccountDrawer}>
-		<Drawer.Trigger class={buttonVariants({ variant: 'outline' })}>Add Account</Drawer.Trigger>
-		<Drawer.Content>
-			<Drawer.Header>
-				<Drawer.Title>Add Account</Drawer.Title>
-			</Drawer.Header>
-
-			<form class="grid items-start gap-4 px-4" onsubmit={addAccount}>
-				<div class="grid gap-2">
-					<Label for="name-{id}">Name</Label>
-					<Input
-						id="name-{id}"
-						type="text"
-						placeholder="Account Name"
-						required={true}
-						bind:value={name}
-					/>
-				</div>
-
-				<Button type="submit">Save</Button>
-			</form>
-
-			<Drawer.Footer>
-				<Drawer.Close>Cancel</Drawer.Close>
-			</Drawer.Footer>
-		</Drawer.Content>
-	</Drawer.Root>
+	<button
+		type="button"
+		onclick={() => (showAddAccountDrawer = true)}
+		class={buttonVariants({ variant: 'outline' })}>Add Account</button
+	>
 </div>
+
+<AddAccountDrawer bind:open={showAddAccountDrawer} />
