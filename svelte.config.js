52df7df1dc558df498b073e9a687b6d469ef0cc6
Alexander Lang <alex@cobot.me>
2025-08-14 21:11:41 +0200

    add base path for production

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import process from 'node:process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +10,8 @@ const config = {
 	kit: { adapter: adapter() },
 	compilerOptions: {
 		runes: true
-	}
+	},
+	base: process.env.NODE_ENV === 'production' ? '/ramit2' : ''
 };
 
 export default config;
